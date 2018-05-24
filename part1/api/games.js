45752716b6dfe93327768412eae7cfc50ed6121f
kieranfitz <pte_fitz@hotmail.com>
2018-05-24 16:33:55 +0100

    part1 finished

---
+++ part1/api/games.js
@@ -0,0 +1,32 @@
+export const games = [
+  {
+    'title': 'GTA V',
+    'publisher': 'Rockstar',
+    'age_rating': '18',
+    'product_code': '123',
+	'rating': '4.8',
+  },
+  {
+    'title': 'FIFA 18',
+    'publisher': 'EA',
+    'age_rating': '3',
+    'product_code': '456',
+	'rating': '4.2',
+
+  },
+  {
+    'title': 'Skyrim',
+    'publisher': 'Bethesda',
+    'age_rating': '15',
+    'product_code': '789',
+	'rating': '4.7',
+  },
+  {
+    'title': 'Far Cry 5',
+    'publisher': 'Ubisoft',
+    'age_rating': '18',
+    'product_code': '963',
+	'rating': '3.8',
+
+  },
+];
